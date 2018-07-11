ecbd5f3cac0f9768ff2b7cf33d042fd6d2d34bd6
apple <apple@appledeMacBook-Air-2.local>
2018-07-11 21:53:12 +0800

    version3_li

--- hihi/app_li/views.py
+++ hihi/app_li/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def secreat(request):
+    my_dict = {
+        'secreats': ['secreat1', 'secreat2', 'secreat3'],
+        'answer': 'no secreat!'
+    }
+    return render(request, 'secreat.html', context=my_dict)
