91ec97a9c6b3d9711ab4531743d97cd1b4d1181f
apple <apple@appledeMacBook-Air-2.local>
2018-07-11 22:15:07 +0800

    merge_import

--- hihi/app_li/urls.py
+++ hihi/app_li/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 
-<<<<<<< HEAD
 from .views import secreat
-=======
-from .views import index
->>>>>>> 85bdccbb6f446a398987bccb84f5682185971daf
 
 urlpatterns = [
     path('', secreat),
