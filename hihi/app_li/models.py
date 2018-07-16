b4ec89fac64090221b0049bcb66802db86a6ef22
apple <apple@appledeMacBook-Air-2.local>
2018-07-16 20:50:13 +0800

    hahaha

--- hihi/app_li/models.py
+++ hihi/app_li/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Character (models.Model):
+    battleNet = models.CharField(max_length=12, primary_key=True)
+    profession = models.CharField(max_length=10)
+    level = models.IntegerField()
+
+class Gift
+    aim = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
