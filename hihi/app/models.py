ef6cdfb520ed7bd80552ae132ee1373657109f4b
NormanPunLongCheong <ztobpun@gmail.com>
2018-07-16 20:48:19 +0800

    add norman_model

--- hihi/app/models.py
+++ hihi/app/models.py
@@ -12,3 +12,10 @@ class Store(models.Model):
 
 # models.py > migrations > migrate to db
 #                             ALTER TABLE
+
+class Norman(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    sex = models.BooleanField()
+    phone_number = models.DecimalField(max_digits=10,decimal_places=0)
+    
